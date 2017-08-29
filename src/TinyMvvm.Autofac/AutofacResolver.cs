a050bf04f90f3f2d2329055cae54232b9b5f5848
Daniel Hindrikes <daniel.hindrikes@sogeti.se>
2017-08-29 15:24:07 +0200

    created autofac resolver and forms project

---
+++ src/TinyMvvm.Autofac/AutofacResolver.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using System;
+using TinyMvvm.IoC;
+
+namespace TinyMvvm.Autofac
+{
+    public class AutofacResolver : IResolver
+    {
+        private IContainer _container;
+
+        public AutofacResolver(IContainer container)
+        {
+            _container = container;
+        }
+
+        public T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        public T Resolve<T>(string key)
+        {
+            return _container.ResolveKeyed<T>(key);
+        }
+    }
+}
