93654f973623130455d3defd1f31b0542bd8e1eb
Daniel Hindrikes <daniel@hindrikes.se>
2018-06-23 23:15:06 +0200

    added a set method to ViewModelBase

--- src/TinyMvvm/ViewModelBase.cs
+++ src/TinyMvvm/ViewModelBase.cs
@@ -99,13 +99,20 @@ namespace TinyMvvm
             }
         }
 
-
-
         public void RaisePropertyChanged([CallerMemberName]string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                RaisePropertyChanged(propertyName);
+            }
+        }
     }
 }
