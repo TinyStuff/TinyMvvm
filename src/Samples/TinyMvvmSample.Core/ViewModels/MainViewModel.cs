9596df32ee054f48b531361186caaf1d7ade4e60
Daniel Hindrikes <daniel@hindrikes.se>
2019-11-25 15:14:44 +0100

    More shell stuff

---
+++ src/Samples/TinyMvvmSample.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TinyMvvm;
+
+namespace TinyMvvmSample.Core.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand Contact => new TinyCommand(() =>
+        {
+            Navigation.NavigateToAsync("///about/contact?id=1");
+        });
+
+        public ICommand About => new TinyCommand(() =>
+        {
+            Navigation.NavigateToAsync("///about");
+        });
+
+        public override Task OnAppearing()
+        {
+            return base.OnAppearing();
+        }
+    }
+}
