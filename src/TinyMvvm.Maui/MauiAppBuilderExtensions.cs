71554f63f23d0a40892d56febedb90ae1c29139a
Daniel Hindrikes <daniel@hindrikes.se>
2022-06-21 20:31:04 +0200

    maui cleanup

--- src/TinyMvvm.Maui/MauiAppBuilderExtensions.cs
+++ src/TinyMvvm.Maui/MauiAppBuilderExtensions.cs
@@ -34,41 +34,7 @@ namespace TinyMvvm.Maui
             RegisterAssemblies(builder, viewAssembly, viewModelAssembly);
 
             return builder;
-        }
-        /// <summary>
-        /// Setup TinyMvvm when using Shell or the classic navigation with NavigationPage. Types for Pages and ViewModels will be registered into the Dependency Injection container.
-        /// </summary>
-        /// /// <param name="useClassicNavigation">Set to true to use navigation with NavigationPage</param>
-        /// <param name="viewAssembly">The assembly where the views are located. If not specify it will use the executing assembly.</param>
-        /// <param name="viewModelAssembly">The assembly where the view models are located. If not specify it will use the executing assembly.</param>
-        /// <returns>The Maui App Builder</returns>
-        /// <param name="builder"></param>
-        public static MauiAppBuilder UseTinyMvvm(this MauiAppBuilder builder, bool useClassicNavigation, Assembly? viewAssembly = null, Assembly? viewModelAssembly = null)
-        {
-            if(!useClassicNavigation)
-            {
-                return UseTinyMvvm(builder, viewAssembly, viewModelAssembly);
-            }
-
-            if (viewAssembly == null)
-            {
-                viewAssembly = Assembly.GetExecutingAssembly();
-            }
-
-            if (viewModelAssembly == null)
-            {
-                viewModelAssembly = Assembly.GetExecutingAssembly();
-            }
-
-            var navigationHelper = new ClassicNavigationHelper();
-            navigationHelper.RegisterViewsInAssembly(viewAssembly);
-
-            builder.Services.AddSingleton<INavigationHelper>(navigationHelper);
-
-            RegisterAssemblies(builder, viewAssembly, viewModelAssembly);
-
-            return builder;
-        }
+        }        
 
         private static void RegisterAssemblies(MauiAppBuilder builder, Assembly viewAssembly, Assembly viewModelAssembly)
         {
--- src/TinyMvvm.Maui/ViewBase.cs
+++ src/TinyMvvm.Maui/ViewBase.cs
@@ -1,82 +1,19 @@
 ﻿namespace TinyMvvm.Maui;
 
-[QueryProperty("TinyId", "tinyid")]
 public abstract class ViewBase : ContentPage
 {
-    internal bool CreatedByTinyMvvm { get; set; }
-    public object? NavigationParameter { get; set; }
-    public Dictionary<string, string>? QueryParameters { get; private set; }
     internal SemaphoreSlim ReadLock { get; private set; } = new SemaphoreSlim(1, 1);
     internal protected bool isShellView;
-
-    private string? tinyId;
-
-    /// <summary>
-    /// Internally used by TinyMvvm
-    /// </summary>
-    public string? TinyId
-    {
-        get => tinyId;
-        set
-        {
-            tinyId = value;
-
-            if (TinyId != null && !CreatedByTinyMvvm && BindingContext is IViewModelBase viewModel)
-            {
-                var shellNavigationHelper = (ShellNavigationHelper)NavigationHelper.Current;
-
-                var queryParameters = shellNavigationHelper.GetQueryParameters(TinyId);
-
-                if (queryParameters != null)
-                {
-                    QueryParameters = queryParameters;
-                    viewModel.QueryParameters = queryParameters;
-                }
-
-                var parameters = shellNavigationHelper.GetParameter(TinyId);
-
-                if (parameters != null)
-                {
-                    viewModel.NavigationParameter = parameters;
-                    NavigationParameter = parameters;
-                }
-
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    try
-                    {
-                        await ReadLock.WaitAsync();
-                        await viewModel.Initialize();
-
-                        viewModel.IsInitialized = true;
-                    }
-                    finally
-                    {
-                        ReadLock.Release();
-                    }
-                });
-
-            }
-        }
-    }
-
-    public ViewBase()
-    {
-
-    }
 
     protected async override void OnBindingContextChanged()
     {
         base.OnBindingContextChanged();
 
-        if (!CreatedByTinyMvvm && BindingContext is IViewModelBase viewModel)
-        {
-            SetupUIAction(viewModel);
+        if (BindingContext is IViewModelBase viewModel)
+        {  
             try
             {
-
-
-                if (!isShellView)
+                if (!viewModel.IsInitialized)
                 {
                     await ReadLock.WaitAsync();
                     await viewModel.Initialize();
@@ -100,146 +37,44 @@ public abstract class ViewBase : ContentPage
 
         if (BindingContext is IViewModelBase viewModel)
         {
-            if (TinyId != null)
-            {
-                var shellNavigationHelper = (ShellNavigationHelper)NavigationHelper.Current;
+            onAppearing(){
+                await ReadLock.WaitAsync();
 
-                var queryParameters = shellNavigationHelper.GetQueryParameters(TinyId);
-
-                if (queryParameters != null)
+                if (!viewModel.IsInitialized)
                 {
-                    viewModel.QueryParameters = queryParameters;
-                    QueryParameters = queryParameters;
+                    await viewModel.Initialize();
+                    viewModel.IsInitialized = true;
                 }
 
-                var parameters = shellNavigationHelper.GetParameter(TinyId);
+                await viewModel.OnAppearing();
 
-                if (parameters != null)
-                {
-                    viewModel.NavigationParameter = parameters;
-                    NavigationParameter = parameters;
-                }
-            }
+                ReadLock.Release();
+            };
 
-            if (viewModel != null)
-            {
+            if(MainThread)
+               
+        }
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (BindingContext is IViewModelBase viewModel)
+        {
                 Device.BeginInvokeOnMainThread(async () =>
                 {
-                    await ReadLock.WaitAsync();
-
-                    if (!viewModel.IsInitialized)
-                    {
-                        await viewModel.Initialize();
-                        viewModel.IsInitialized = true;
-                    }
+                    await ReadLock.WaitAsync();                 
 
                     await viewModel.OnAppearing();
 
                     ReadLock.Release();
                 });
-            }
-        }
-    }
-
-    internal static void SetupUIAction(IViewModelBase viewModel)
-    {
-        if (viewModel is ViewModelBase viewModelBase)
-        {
-
-            if (viewModelBase.BeginInvokeOnMainThread == null)
-            {
-                viewModelBase.BeginInvokeOnMainThread = (action) =>
-                {
-                    Device.BeginInvokeOnMainThread(action);
-                };
-            }
         }
     }
 
     internal virtual void CreateViewModel()
     {
 
-    }
-}
-
-public abstract class ViewBase<T> : ViewBase where T : IViewModelBase?
-{
-    public T? ViewModel
-    {
-        get
-        {
-            if (BindingContext != null)
-            {
-                return (T)BindingContext;
-            }
-
-            return default(T);
-        }
-    }
-
-    internal override void CreateViewModel()
-    {
-        if (Resolver.IsEnabled)
-        {
-            BindingContext = Resolver.Resolve(typeof(T));
-        }
-        else
-        {
-            BindingContext = Activator.CreateInstance(typeof(T));
-        }
-    }
-
-    public ViewBase(bool isShellView)
-    {
-        this.isShellView = isShellView;
-
-        if (isShellView)
-        {
-            CreateViewModel();
-        }
-    }
-
-    public ViewBase()
-    {
-
-        var navigation = (ClassicNavigationHelper)NavigationHelper.Current;
-
-        if (!(navigation.ViewCreator is TinyMvvmViewCreator))
-        {
-            navigation.ViewCreator = new TinyMvvmViewCreator();
-        }
-    }
-
-    protected override void OnAppearing()
-    {
-        if (ViewModel == null && !CreatedByTinyMvvm)
-        {
-            CreateViewModel();
-        }
-
-        base.OnAppearing();
-    }
-
-    protected override void OnDisappearing()
-    {
-        base.OnDisappearing();
-
-        if (ViewModel is IViewModelBase viewModel)
-        {
-            if (viewModel != null)
-            {
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    await viewModel.OnDisappearing();
-                });
-            }
-        }
-    }
-}
-
-public class ShellViewBase<T> : ViewBase<T> where T : IViewModelBase?
-{
-    public ShellViewBase() : base(true)
-    {
     }
 }
